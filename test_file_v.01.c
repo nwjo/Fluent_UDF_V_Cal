29b93fe7c8ada18297a32aa34b5caee497312210
nwjo <40313625+nwjo@users.noreply.github.com>
2023-03-18 21:21:15 +0900

    change for loop position...not working(force termination)

--- test_file_v.01.c
+++ test_file_v.01.c
@@ -10,30 +10,31 @@ DEFINE_ON_DEMAND(on_demnad_calc)
     /* Integrate Tangential Velocity */
     real sum_v;
     real sum_vol;
-    real sum_ave_v[ND_ND];
+    real sum_ave_v[3];
     real x[ND_ND];
     real radius;
     real radial_coordinate;
     real theta_speed;
-    real tangetial_v;
+    real tangential_v;
     int i;
 
     cell_t c;
     d = Get_Domain(1);
-
-    for(i = 0; i<4; i++) 
-    {
 
         thread_loop_c(t, d)
         {
 
             if (FLUID_THREAD_P(t))
             {
+
+                for(i = 0; i<4; i++) 
+                {
+
                 radius = 0.0085;
-                radius += (real) i*0.001; 
+                radius += i*0.001;
+
                 begin_c_loop_int(c,t)
                 {
-                    
                     C_CENTROID(x,c,t);
                     if (sqrt(pow(x[0],2) + pow(x[1],2))>=radius && sqrt(pow(x[0],2) + pow(x[1],2))< radius+2.0e-6 )
                     {
@@ -47,22 +48,30 @@ DEFINE_ON_DEMAND(on_demnad_calc)
                     else
                     sum_v += 0;
                     sum_vol += 0;
+
                 end_c_loop_int(c,t)
                 }
-            sum_ave_v[i] = sum_v/sum_vol;
+
+                sum_ave_v[i] = sum_v/sum_vol;
+
+                #if RP_NODE
+                sum_ave_v[i] = PRF_GRSUM1(sum_ave_v[i]);
+                #endif /*RP_NODE*/
+
+                #if PARALLEL
+                if(I_AM_NODE_ZERO_P)
+                #endif /*PARALLEL*/
+
+
+
+                printf("Volume Averaged Velocity: %g\n", sum_ave_v[i]);
+                fflush(stdout);
+                }
+
+
+
             }
-        #if RP_NODE
-        sum_ave_v[i] = PRF_GRSUM1(sum_ave_v[i]);
-        #endif
+        } 
 
-        }
-
-    #if PARALLEL
-    if(I_AM_NODE_ZERO_P)
-    #endif
-    printf("Volume Averaged Velocity: %g\n", sum_ave_v[i]);
-    fflush(stdout);
-
-    #endif
-    }
+    #endif /*RP_HOST*/
 }
