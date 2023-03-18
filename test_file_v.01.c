d01985d0e2f047b672a8e8ee677a518805b7a95b
nwjo <40313625+nwjo@users.noreply.github.com>
2023-03-18 20:12:24 +0900

    add few definitions

--- test_file_v.01.c
+++ test_file_v.01.c
@@ -10,49 +10,57 @@ DEFINE_ON_DEMAND(on_demnad_calc)
     /* Integrate Velocity Magnitude */
     real sum_v;
     real sum_vol;
-    real sum_ave_v;
+    real sum_ave_v[ND_ND];
     real x[ND_ND];
     real radius;
+    int i;
+    //real theta=atan(x[1]/x[0]);
     //real radial_v;
     //real tangetial_v;
     cell_t c;
     d = Get_Domain(1);
 
-    thread_loop_c(t, d)
+    for(i = 0; i<4; i++) 
     {
-        if (FLUID_THREAD_P(t))
+
+        thread_loop_c(t, d)
         {
-            begin_c_loop_int(c,t)
+
+            if (FLUID_THREAD_P(t))
             {
                 radius = 0.0085;
-                C_CENTROID(x,c,t);
-                if (sqrt(pow(x[0],2) + pow(x[1],2))>=radius && sqrt(pow(x[0],2) + pow(x[1],2))< radius+2.0e-6 )
+                radius += (real) i*0.001; 
+                begin_c_loop_int(c,t)
                 {
-                    //radial_v = sqrt(C_U(c,t)*C_U(c,t)+C_V(c,t)*C_V(c,t));
-                    //tangential_v = atan2(C_W(c,t)/C_U(c,t));
+                    
+                    C_CENTROID(x,c,t);
+                    if (sqrt(pow(x[0],2) + pow(x[1],2))>=radius && sqrt(pow(x[0],2) + pow(x[1],2))< radius+2.0e-6 )
+                    {
+                        //radial_v = sqrt(C_U(c,t)*C_U(c,t)+C_V(c,t)*C_V(c,t));
+                        //tangential_v = atan2(C_W(c,t)/C_U(c,t));
 
-                    sum_v += sqrt(C_U(c,t)*C_U(c,t)+C_V(c,t)*C_V(c,t))*C_VOLUME(c,t);
-                    sum_vol += C_VOLUME(c,t);
+                        sum_v += sqrt(C_U(c,t)*C_U(c,t)+C_V(c,t)*C_V(c,t))*C_VOLUME(c,t);
+                        sum_vol += C_VOLUME(c,t);
+                    }
+                    else
+                    sum_v += 0;
+                    sum_vol += 0;
+                end_c_loop_int(c,t)
                 }
-                else
-                sum_v += 0;
-                sum_vol += 0;
-
-            end_c_loop_int(c,t)
+            sum_ave_v[i] = sum_v/sum_vol;
             }
-            sum_ave_v = sum_v/sum_vol;
-        }
-
         #if RP_NODE
-        sum_ave_v = PRF_GRSUM1(sum_ave_v);
+        sum_ave_v[i] = PRF_GRSUM1(sum_ave_v[i]);
         #endif
-    }
+
+        }
 
     #if PARALLEL
     if(I_AM_NODE_ZERO_P)
     #endif
-    printf("Volume Averaged Velocity: %g\n", sum_ave_v);
+    printf("Volume Averaged Velocity: %g\n", sum_ave_v[i]);
     fflush(stdout);
 
     #endif
+    }
 }
